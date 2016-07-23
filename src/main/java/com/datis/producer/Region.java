614783f084e0be5f0483de8918ef57374fd870e6
jeus <alkhandani@gmail.com>
2016-07-23 13:13:34 +0430

    add method for get country

--- src/main/java/com/datis/producer/Region.java
+++ src/main/java/com/datis/producer/Region.java
@@ -4,10 +4,29 @@
 */
 package com.datis.producer;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  *
  * @author jeus
  */
 public class Region {
+    
+    String[] world =  "USA,Afghanistan,Albania,Algeria".split(",");
+public void Region()
+{
+    
+}
+    
+
+
+
+    private String getWord()
+    {
+        Random random = new Random();
+        return world[random.nextInt(world.length)];
+    }
     
 }
