da30cdf972a01aba2681de2c2385402521e013c1
jeus <alkhandani@gmail.com>
2016-07-23 13:00:50 +0430

    First Commit

---
+++ src/main/java/com/datis/producer/Region.java
@@ -0,0 +1,13 @@
+/*
+*This class for send every Second a Word to specified topic
+*i will with this want test windowing. 
+*/
+package com.datis.producer;
+
+/**
+ *
+ * @author jeus
+ */
+public class Region {
+    
+}
